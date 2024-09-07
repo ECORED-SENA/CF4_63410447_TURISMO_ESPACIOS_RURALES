957ab8ea66bd7a0fd1ddb4510f02c797f793b816
edwsvesu <70658133+edwsvesu@users.noreply.github.com>
2024-09-07 10:12:59 -0500

    Initial commit

---
+++ src/views/Actividad.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno(icono="far fa-question-circle" titulo="Actividad didáctica")
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    #Actividad                
+      <Actividad :cuestionario="cuestionario" />
+
+</template>
+
+<script>
+import Actividad from '@/components/actividad/Actividad'
+export default {
+  name: 'ActividadDidactica',
+  components: { Actividad },
+  data: () => ({
+    cuestionario: {
+      tema: 'Manejo de los residuos sólidos en la producción avícola.',
+      titulo: 'Cuestionario',
+      introduccion:
+        'Lea cada enunciado y luego seleccione la respuesta correcta según corresponda.',
+      barajarPreguntas: false,
+      preguntas: [
+        {
+          id: 1,
+          texto:
+            'Cuáles son los tipos de explotación en los sistemas productivos avícolas:',
+          imagen: require('@/assets/componentes/pregunta_1.svg'),
+          barajarRespuestas: true,
+          opciones: [
+            { id: 'a', texto: '', esCorrecta: true },
+            { id: 'b', texto: '', esCorrecta: false },
+            { id: 'c', texto: '', esCorrecta: false },
+            { id: 'd', texto: '', esCorrecta: false },
+          ],
+          mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
+          mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
+        },
+      ],
+      mensaje_final_aprobado: '¡Excelente! Ha superado la actividad.',
+      mensaje_final_reprobado:
+        'Le recomendamos volver a revisar el componente formativo e intentar nuevamente la actividad didáctica.',
+    },
+  }),
+  computed: {},
+  methods: {},
+}
+</script>
